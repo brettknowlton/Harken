4dc78bffa01d160639db189ea7e96cd7ffac6500
brettknowlton <brettlknowlton@gmail.com>
2024-03-25 06:47:45 -0600

    MainMenu Systems
    Added systems to display a (pretty archaic) main menu screen- mostly copied from bevy's game_menu example.
    Code is functional and compiles without errors.

--- src/menu/mod.rs
+++ src/menu/mod.rs
@@ -1,6 +1,8 @@
-use bevy::prelude::*;
+use bevy::{app::AppExit, prelude::*};
 use super::{despawn_screen, GameState};
 
+
+const TEXT_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
 
 #[derive(Clone, Copy, Default, Eq, PartialEq, Debug, Hash, States)]
 enum MainMenuState {
@@ -11,21 +13,216 @@ enum MainMenuState {
 }
 
 pub fn main_menu_plugin(app: &mut App) {
-    app
+    app 
         .init_state::<MainMenuState>()
         .add_systems(OnEnter(GameState::MainMenu), main_menu_setup)
 
         .add_systems(OnEnter(MainMenuState::Splash), splash_setup)
         .add_systems(Update, countdown.run_if(in_state(MainMenuState::Splash)))
-        .add_systems(OnExit(MainMenuState::Splash), despawn_screen::<OnSplashScreen>);
+        .add_systems(OnExit(MainMenuState::Splash), despawn_screen::<OnSplashScreen>)
+
+        .add_systems(OnEnter(MainMenuState::Main), main_menu_setup);
 
     }
 
-fn main_menu_setup(
+// Tag component used to tag entities added on the main menu screen
+#[derive(Component)]
+struct OnMainMenu;
 
+// Tag component used to tag entities added on the settings menu screen
+#[derive(Component)]
+struct OnSettingsMenu;
+
+// Tag component used to tag entities added on the display settings menu screen
+#[derive(Component)]
+struct OnDisplaySettingsMenu;
+
+// Tag component used to tag entities added on the sound settings menu screen
+#[derive(Component)]
+struct OnSoundSettingsMenu;
+
+//colors of buttons in different states
+const NORMAL_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
+const HOVERED_BUTTON: Color = Color::rgb(0.25, 0.25, 0.25);
+const HOVERED_PRESSED_BUTTON: Color = Color::rgb(0.25, 0.65, 0.25);
+const PRESSED_BUTTON: Color = Color::rgb(0.35, 0.75, 0.35);
+
+// Tag component used to mark which setting is currently selected
+#[derive(Component)]
+struct SelectedOption;
+
+// All actions that can be triggered from a button click
+#[derive(Component)]
+enum MenuButtonAction {
+    Play,
+    Continue,
+    Settings,
+    SettingsDisplay,
+    SettingsSound,
+    BackToMainMenu,
+    BackToSettings,
+    Quit,
+}
+
+fn main_menu_setup(
+    mut commands: Commands
+    , asset_server: Res<AssetServer>
 ){
     println!("Entered Main Menu");
-}
+
+    // Common style for all buttons on the screen
+    let button_style = Style {
+        width: Val::Px(250.0),
+        height: Val::Px(65.0),
+        margin: UiRect::all(Val::Px(20.0)),
+        justify_content: JustifyContent::Center,
+        align_items: AlignItems::Center,
+        ..default()
+    };
+    let button_icon_style = Style {
+        width: Val::Px(30.0),
+        // This takes the icons out of the flexbox flow, to be positioned exactly
+        position_type: PositionType::Absolute,
+        // The icon will be close to the left border of the button
+        left: Val::Px(10.0),
+        ..default()
+    };
+    let button_text_style = TextStyle {
+        font_size: 40.0,
+        color: TEXT_COLOR,
+        ..default()
+    };
+
+    commands
+        .spawn((
+            NodeBundle {
+                style: Style {
+                    width: Val::Percent(100.0),
+                    height: Val::Percent(100.0),
+                    align_items: AlignItems::Center,
+                    justify_content: JustifyContent::Center,
+                    ..default()
+                },
+                ..default()
+            },
+            OnMainMenu,
+        ))
+        .with_children(|parent| {
+            parent
+                .spawn(NodeBundle {
+                    style: Style {
+                        flex_direction: FlexDirection::Column,
+                        align_items: AlignItems::Center,
+                        ..default()
+                    },
+                    background_color: Color::CRIMSON.into(),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    // Display the game name
+                    parent.spawn(
+                        TextBundle::from_section(
+                            "Bevy Game Menu UI",
+                            TextStyle {
+                                font_size: 80.0,
+                                color: TEXT_COLOR,
+                                ..default()
+                            },
+                        )
+                        .with_style(Style {
+                            margin: UiRect::all(Val::Px(50.0)),
+                            ..default()
+                        }),
+                    );
+
+                    // Display three buttons for each action available from the main menu:
+                    // - new game
+                    // - settings
+                    // - quit
+                    parent
+                        .spawn((
+                            ButtonBundle {
+                                style: button_style.clone(),
+                                background_color: NORMAL_BUTTON.into(),
+                                ..default()
+                            },
+                            MenuButtonAction::Play,
+                        ))
+                        .with_children(|parent| {
+                            let icon = asset_server.load("textures/Game Icons/right.png");
+                            parent.spawn(ImageBundle {
+                                style: button_icon_style.clone(),
+                                image: UiImage::new(icon),
+                                ..default()
+                            });
+                            parent.spawn(TextBundle::from_section(
+                                "New Game",
+                                button_text_style.clone(),
+                            ));
+                        });
+                    parent
+                        .spawn((
+                            ButtonBundle {
+                                style: button_style.clone(),
+                                background_color: NORMAL_BUTTON.into(),
+                                ..default()
+                            },
+                            MenuButtonAction::Continue,
+                        ))
+                        .with_children(|parent|{
+                            let icon = asset_server.load("textures/Game Icons/right.png");
+                            parent.spawn(ImageBundle {
+                                style: button_icon_style.clone(),
+                                image: UiImage::new(icon),
+                                ..default()
+                            });
+                            parent.spawn(TextBundle::from_section(
+                                "Continue",
+                                button_text_style.clone(),
+                            ));
+                        });
+                    parent
+                        .spawn((
+                            ButtonBundle {
+                                style: button_style.clone(),
+                                background_color: NORMAL_BUTTON.into(),
+                                ..default()
+                            },
+                            MenuButtonAction::Settings,
+                        ))
+                        .with_children(|parent| {
+                            let icon = asset_server.load("textures/Game Icons/wrench.png");
+                            parent.spawn(ImageBundle {
+                                style: button_icon_style.clone(),
+                                image: UiImage::new(icon),
+                                ..default()
+                            });
+                            parent.spawn(TextBundle::from_section(
+                                "Settings",
+                                button_text_style.clone(),
+                            ));
+                        });
+                    parent
+                        .spawn((
+                            ButtonBundle {
+                                style: button_style,
+                                background_color: NORMAL_BUTTON.into(),
+                                ..default()
+                            },
+                            MenuButtonAction::Quit,
+                        ))
+                        .with_children(|parent| {
+                            let icon = asset_server.load("textures/Game Icons/exitRight.png");
+                            parent.spawn(ImageBundle {
+                                style: button_icon_style,
+                                image: UiImage::new(icon),
+                                ..default()
+                            });
+                            parent.spawn(TextBundle::from_section("Quit", button_text_style));
+                        });
+                });
+        });
+    }
 
 #[derive(Component)]
 struct OnSplashScreen;
@@ -69,11 +266,11 @@ fn splash_setup(mut commands: Commands, asset_server: Res<AssetServer>) {
 
 //this counts down and progresses the lifetime of the splash screen (2 seconds?)
 fn countdown(
-    mut game_state: ResMut<NextState<MainMenuState>>,
+    mut menu_state: ResMut<NextState<MainMenuState>>,
     time: Res<Time>,
     mut timer: ResMut<SplashTimer>,
 ){
     if timer.tick(time.delta()).finished() {
-        game_state.set(MainMenuState::Main);
+        menu_state.set(MainMenuState::Main);
     }
 }
